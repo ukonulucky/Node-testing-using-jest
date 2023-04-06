63663a4d1ddd267ef8c154c8cb02091a8c44588b
ukonulucky <ukonulucky@gmail.com>
2023-04-06 12:34:45 +0100

    node testing

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000
+
+
+
+appl.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`)
+})
